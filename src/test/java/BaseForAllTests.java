570e7e3335b73f74e8dc59b5fc3409381e903d92
Mariya Kazakova <Mariya_Kazakova@epam.com>
2020-07-22 01:30:45 +0600

    delete comment

--- src/test/java/BaseForAllTests.java
+++ src/test/java/BaseForAllTests.java
@@ -14,12 +14,5 @@ public class BaseForAllTests {
         System.setProperty("webdriver.gecko.driver", "./src/main/resources/gecko/geckodriver.exe");
         driver = new FirefoxDriver();
         driver.get(baseUrl); // Open the www.wildberries.kz. in the FireFox browser
-
-    /*  driver.findElement(By.xpath("//div[@class='user-menu-item']")).click();
-        WebElement phone = driver.findElement(By.xpath("//*[@id='phoneMobile']"));
-        phone.sendKeys("7787101123");
-        driver.findElement(By.xpath("//div[@class='i-form-block-v1']/div/button[@data-href]")).click();
-        Thread.sleep(20000); */
-
     }
 }
