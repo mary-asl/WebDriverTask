44bd711533edb264d3e4d316ab85b4ae855e6a88
Mariya Kazakova <60565206+mary-asl@users.noreply.github.com>
2020-07-23 02:29:37 +0600

    update Base for all tests file

--- src/test/java/BaseForAllTests.java
+++ src/test/java/BaseForAllTests.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseForAllTests {
 
@@ -8,9 +11,16 @@ public class BaseForAllTests {
     protected String baseUrl = "https://www.wildberries.kz";
 
     @BeforeClass
-    public void initWebDriver() throws InterruptedException {
+    public void initWebDriver() {
         System.setProperty("webdriver.gecko.driver", "./src/main/resources/gecko/geckodriver.exe");
         driver = new FirefoxDriver();
         driver.get(baseUrl); // Open the www.wildberries.kz. in the FireFox browser
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void close(){
+        driver.close();
     }
 }
